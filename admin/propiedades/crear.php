4bde9898655ff6b6d0c0ef77eb83693b7184af32
Sara Restrepo Vélez <89092194+SaraRestrepoV@users.noreply.github.com>
2022-01-09 16:19:25 -0600

    Conectando la base de datos

--- admin/propiedades/crear.php
+++ admin/propiedades/crear.php
@@ -1,4 +1,10 @@
 <?php
+    //base de datos
+
+    require '../../includes/config/database.php';
+
+    $db = conectarDB();
+
     require '../../includes/funciones.php';
     incluirTemplate('header');
 ?>
@@ -20,6 +26,34 @@
 
                 <label for="imagen">Imagen:</label>
                 <input type="file" id="imagen" accept="image/jpej, image/png">
+
+                <label for="descripcion">Descripción:</label>
+                <textarea id="descripcion"></textarea>
+            </fieldset>
+
+            <fieldset>
+                <legend>Información de la propiedad</legend>
+
+                <label for="habitaciones">Habitaciones:</label>
+                <input type="number" id="habitaciones" placeholder="Ej: 3" min="1" max="9">
+
+                <label for="baños">Baños:</label>
+                <input type="number" id="baños" placeholder="Ej: 3" min="1" max="9">
+
+                <label for="estacionamiento">Estacionamiento:</label>
+                <input type="number" id="estacionamiento" placeholder="Ej: 3" min="1" max="9">
+
+            </fieldset>
+
+            <fieldset>
+                <legend>Vendedor</legend>
+
+                <select>
+                    <option value="1">Karen</option>
+                    <option value="2">Sara</option>
+                </select>
+
+                <input type="submit" value="Crear Propiedad" class="boton boton-verde">
             </fieldset>
         </form>
     </main>
