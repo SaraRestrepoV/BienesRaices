fa06aa49cb85722dc1a60132538a68c6700e34ce
Sara Restrepo Vélez <89092194+SaraRestrepoV@users.noreply.github.com>
2022-01-19 14:31:54 -0600

    eliminar propiedades de admin

--- admin/index.php
+++ admin/index.php
@@ -11,7 +11,31 @@
     $resultadoConsulta = mysqli_query($db, $query);
 
     //Muestra mensaje condicional
-    $resultado = $_GET['resultado'];
+    $resultado = $_GET['resultado'] ?? null;
+
+    if($_SERVER['REQUEST_METHOD'] === 'POST') {
+        $id = $_POST['id'];
+        $id = filter_var($id, FILTER_VALIDATE_INT);
+
+        if($id) {
+            //Eliminar el archivo
+            
+            $query = "SELECT imagen FROM propiedades WHERE id = ${id}";
+            $resultado = mysqli_query($db, $query);
+            $propiedad = mysqli_fetch_assoc($resultado);
+
+            unlink('../imagenes/' . $propiedad['imagen']);
+
+            //Eliminar la propiedad
+            $query = "DELETE FROM propiedades WHERE id = ${id}";
+            $resultado = mysqli_query($db, $query);
+
+            if($resultado) {
+                header('location: /admin');
+            }
+        }
+    }
+
     //Incluye el template del header
     require '../includes/funciones.php';
     incluirTemplate('header');
@@ -46,10 +70,10 @@
                     <td><img src="../imagenes/<?php echo $propiedad['imagen']  . '.jpg' ?>" class="imagen-tabla"></td>
                     <td>$ <?php echo $propiedad['precio']; ?></td>
                     <td>
-                        <form>
-                            <input class="boton-rojo-block" type="submit" value="Eliminar">
+                        <form method="POST" class="w-100">
+                            <input type="hidden" name="id" value="<?php echo $propiedad['id'];?>">
+                            <input class="boton-rojo-block" type="submit" value="Eliminar+">
                         </form>
-                        <a href="#" class="boton-rojo-block">Eliminar</a>
                         <a href="/admin/propiedades/actualizar.php?id=<?php echo $propiedad['id'];?>" class="boton-amarillo-block">Actualizar</a>
                     </td>
                 </tr>
